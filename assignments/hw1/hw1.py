eaca9f5bb10de76fde9d650d9ae8ec0ca31eec45
Elizabeth Maverick <mavericke@g.cofc.edu>
2022-01-28 15:31:39 -0500

    Submitting homework one. Thank you professor.

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,51 @@
 """
-Name: <your name goes here – first and last>
+Name: <Elizabeth Maverick>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Homework 1>
 
 Certification of Authenticity:
 <include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is entirely my own work. I discussed questions 1 and 2 with Brooke.
+
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
+
 
 
 def shooting_percentage():
-    pass
-
+    total_shots = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter the player's made shots: "))
+    shot_percentage = shots_made / total_shots * 100
+    print("Shooting_percentage =", shot_percentage)
 
 def coffee():
-    pass
+    # get cost of coffee per lb including shipping
+    coffee_lbs = eval(input("Enter total lbs of coffee "))
+    coffee_cost = coffee_lbs * (10.50 + .86) + 1.50
+    # display result to user
+    print("Coffee =", coffee_cost)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("Enter total number of kilometers "))
+    miles = kilometers / 1.61
+    print ("Kilometers_to_miles =", miles)
+
 
 
 if __name__ == '__main__':
